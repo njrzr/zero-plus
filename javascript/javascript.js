fa1b1e521d5f05c08ff706beaea118dc3442cf8a
Orlando Pernia <69866332+njrzr@users.noreply.github.com>
2020-08-24 23:14:02 -0400

    Actualizacion de archivos de imagen, y arreglos a la pagina para que sea web responsive

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,28 @@
-const myId = 'https://es.gravatar.com/angelus078.json';
+let myGravatar = 'https://en.gravatar.com/angelus078.json';
+
+//Using XMLHttpRequest()
+/*let request = new XMLHttpRequest();
+
+request.open("GET", myGravatar, true);
+request.onload = function() {
+	let data = JSON.parse(this.response);
+	let avatar = data.entry[0].photos[0].value;
+	let logo = document.getElementById("logo");
+	logo.style.background = `url(${avatar})`;
+	logo.style.backgroundSize = "cover";
+}
+request.send()
+
+//Using the fetch api
+fetch(myGravatar)
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		let address = data.entry[0].currency[0].value;
+		let construction = document.getElementById("construction");
+		construction.innerHTML = `${address}`;
+	})
+	.catch((err) => {
+		console.log(err);
+	})*/
